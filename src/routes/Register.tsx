ff301544a33f2d57d9c528863ac3d72e59e4ccd6
EloyGarcia <soyeloygarciamartinez@gmail.com>
2025-02-22 14:52:08 +0100

    restyle register an login forms

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -6,8 +6,11 @@ import { signInWithGooglePopup } from '../utils/firebase.utils';
 import logogoogle from '../assets/logogoogle.png';
 import logoapple from '../assets/logoapple.png';
 
+import show from '../assets/show.png';
+import hide from '../assets/hide.png';
+
 const Register = () => {
-    const [isRegistering, setIsRegistering] = useState(false);
+    const [isSignUpMode, setIsSignUpMode] = useState(true);
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -16,25 +19,26 @@ const Register = () => {
     const navigate = useNavigate();
     const userContext = useContext(UserContext);
     const user = userContext ? userContext.user : null;
-    const setUser = userContext ? userContext.setUser : () => {};
+    const setUser = userContext ? userContext.setUser : () => { };
     const errorMessage = document.getElementsByClassName('error');
+    const [showPassword, setShowPassword] = useState(false);
 
     //registro
-    const handleRegister = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const register = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();  //para evitar el comportamiento default del evento, del form, (enviarlo y recargar pag)
         try {
             const auth = getAuth();
             const { user } = await createUserWithEmailAndPassword(auth, email, password);
             setUser(user);
         } catch (error) {
             Array.from(errorMessage).forEach((element) => {
-                element.textContent = 'Error en el registro';
+                element.textContent = 'Error en el registro.';
             });
         }
     };
 
     //inicar sesion
-    const handleSignIn = async (event: React.FormEvent<HTMLFormElement>) => {
+    const login = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         try {
             const auth = getAuth();
@@ -43,83 +47,154 @@ const Register = () => {
             navigate(-1);
         } catch (error) {
             Array.from(errorMessage).forEach((element) => {
-                element.textContent = 'Error al iniciar sesion: email y/o contraseña incorrectos.'; {/* + error */}
+                element.textContent = 'Error al iniciar sesion: email y/o contraseña incorrectos.'; {/* + error */ }
             });
         }
     };
 
     //inicio de sesion con google
-    const logGoogleUser = async () => {
+    const loginGoogle = async () => {
         try {
             const response = await signInWithGooglePopup();
             setUser(response.user);
         } catch (error) {
             Array.from(errorMessage).forEach((element) => {
                 element.textContent = 'Error al iniciar sesion con Google.';
-            });        }
+            });
+        }
+    };
+
+    //mostrar contraseña
+    const showPassWord = () => {
+        setShowPassword(!showPassword);
     };
 
     return (
-        <div className="register container mx-auto mt-5 mb-5">
-            <div className="row">
-                <div className="col-12 col-md-6 bg-black text-white p-5 position-relative text-end">
-                    <h2>{isRegistering ? 'Registro' : 'Iniciar Sesión'}</h2>
-                    <p>{isRegistering ? 'Registrate y consigue un 10% de descuento' : 'Inicia sesión para poder comprar'}</p>
-                    <h1 className='position-absolute bottom-0 text-opacity-100'>SneakVault</h1>
-                </div>
-                <div className="col-12 col-md-6 border border-2 border-black py-5 d-flex flex-column justify-content-center">
+        <div className="register my-5 p-5">
+            <div className="row mx-auto">
+                <div className="mx-auto col-12 col-md-6 border border-2 border-black py-5 d-flex text-center flex-column justify-content-center">
+                    <h3 className='fw-bold mt-4'>{isSignUpMode ? 'INICIA SESIÓN CON TU CUENTA' : 'CREAR UNA CUENTA'}</h3>
+                    <h6 className='my-3'>{isSignUpMode ? 'Accede con tu usuario (e-mail) y contraseña' : 'Registrate y consigue un 10% de descuento'}</h6>
                     {user ? (
                         <Navigate to='/home' replace />
                     ) : (
-                        <Form method='post' onSubmit={isRegistering ? handleRegister : handleSignIn}>
-                            <div className="container d-flex flex-column justify-content-center gap-4">
-                                {isRegistering && (
-                                    <div className='form-field w-50 d-flex gap-0'>
-                                        <label htmlFor='username' id='username'>Nombre de Usuario *</label>
-                                        <input type='text' name='username' id='username' required value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <Form method='post' onSubmit={isSignUpMode ? login : register}>
+                            <div className="my-3 d-flex flex-column justify-content-center gap-4">
+                                {isSignUpMode ? (
+                                    <>
+                                        <div className='form-field w-75 d-flex gap-0 mx-auto'>
+                                            <label htmlFor='email' id='email'><h6>Dirección de correo electrónico *</h6></label>
+                                            <input type='text' className='p-1' name='email' id='email' required value={email} onChange={(e) => setEmail(e.target.value)} />
+                                        </div>
+                                        <div className='form-field w-75 d-flex gap-0 mx-auto'>
+                                        <label htmlFor='password' id='password'><h6>Contraseña *</h6></label>
+                                        <div>
+                                            <input
+                                                type={showPassword ? 'text' : 'password'}
+                                                className='p-1 w-75'
+                                                name='password'
+                                                id='password'
+                                                required
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                            />
+                                            <button type='button' className='w-25 button-password py-1' onClick={showPassWord}>
+                                                <img width={20} src={showPassword ? hide : show}/>
+                                            </button>
+                                        </div>
                                     </div>
-                                )}
-                                <div className='form-field w-50 d-flex gap-0'>
-                                    <label htmlFor='email' id='email'>Email *</label>
-                                    <input type='text' name='email' id='email' required value={email} onChange={(e) => setEmail(e.target.value)} />
-                                </div>
-                                <div className='form-field w-50 d-flex gap-0'>
-                                    <label htmlFor='password' id='password'>Contraseña *</label>
-                                    <input type='password' name='password' id='password' required value={password} onChange={(e) => setPassword(e.target.value)} />
-                                </div>
-                                {isRegistering && (
-                                <>
-                                <div className='form-field w-50 d-flex gap-0'>
-                                    <label htmlFor='name' id='name'>Nombre y apellidos *</label>
-                                    <input type='text' name='name' id='name' required value={name} onChange={(e) => setName(e.target.value)} />
-                                </div>
-                                <div className='form-field w-50 d-flex gap-0'>
-                                    <label htmlFor='date' id='date'>Fecha de Nacimiento *</label>
-                                    <input type='date' name='date' id='date' required value={date} onChange={(e) => setDate(e.target.value)} />
-                                </div>
-                                </>
-                                )}
-                                <button type='submit' className='button fw-regular'>{isRegistering ? 'Registrar' : 'Iniciar Sesión'}</button>
-                                </div>
+                                    </>
+                                )
+                                    :
+                                    <>
+                                    
+                                    <div className='form-field'>
+                                        <label htmlFor='gender'><h6>Género </h6></label>
+                                        <div className='form-field w-75 d-flex justify-content-start flex-row gap-4 mx-auto'>
+                                            <div>
+                                                <input type='radio' name='gender' id='hombre' value='Hombre'/>
+                                                <label htmlFor='hombre'><h6 className='text-black-50 mx-1 fw-light'>Hombre</h6></label>
+                                            </div>
+                                            <div>
+                                                <input type='radio' name='gender' id='mujer' value='Mujer'/>
+                                                <label htmlFor='mujer'><h6 className='text-black-50 mx-1 fw-light'>Mujer</h6></label>
+                                            </div>
+                                            <div>
+                                                <input type='radio' name='gender' id='otro' value='Otro'/>
+                                                <label htmlFor='otro'><h6 className='text-black-50 mx-1 fw-light'>Otro</h6></label>
+                                            </div>
+                                        </div>
+                                        </div>
+
+                                        <div className='form-field  w-75 d-flex gap-0 mx-auto'>
+                                            <label htmlFor='email' id='email'><h6>Email *</h6></label>
+                                            <input type='text' className='p-1'  name='email' id='email' required value={email} onChange={(e) => setEmail(e.target.value)} />
+                                        </div>
+
+                                        <div className='form-field w-75 d-flex gap-0 mx-auto'>
+                                        <label htmlFor='password' id='password'><h6>Contraseña *</h6></label>
+                                        <div>
+                                            <input
+                                                type={showPassword ? 'text' : 'password'}
+                                                className='p-1 w-75'
+                                                name='password'
+                                                id='password'
+                                                required
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                            />
+                                            <button type='button' className='w-25 button-password py-1' onClick={showPassWord}>
+                                                <img width={20} src={showPassword ? hide : show}/>
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                        <div className='form-field  w-75 d-flex gap-0 mx-auto'>
+                                            <label htmlFor='username' id='username'><h6>Nombre de Usuario *</h6></label>
+                                            <input type='text' className='p-1'  name='username' id='username' required value={username} onChange={(e) => setUsername(e.target.value)} />
+                                        </div>
+                                        <div className='form-field  w-75 d-flex gap-0 mx-auto'>
+                                            <label htmlFor='name' id='name'><h6>Nombre y apellidos *</h6></label>
+                                            <input type='text' className='p-1'  name='name' id='name' required value={name} onChange={(e) => setName(e.target.value)} />
+                                        </div>
+                                        <div className='form-field  w-75 d-flex gap-0 mx-auto'>
+                                            <label htmlFor='date' id='date'><h6>Fecha de Nacimiento *</h6></label>
+                                            <input type='date' className='p-1'  name='date' id='date' required value={date} onChange={(e) => setDate(e.target.value)} />
+                                        </div>
+
+                                        <div className='form-field w-75 d-flex justify-content-start align-items-start flex-row gap-1 mx-auto mt-3'>
+                                                <input type='checkbox' name='terms' id='terms'/>
+                                                <label htmlFor='terms'><h6 className='text-black-50 mx-1 fw-light'>Acepto los terminos y condiciones.</h6></label>
+                                        </div>
+
+
+                                    </>
+                                }
+                                <div className="error"></div>
+                                <button type='submit' className='button fw-regular w-75 mx-auto'>{isSignUpMode ? 'Iniciar Sesión' : 'Registrarse'}</button>
+                            </div>
                         </Form>
                     )}
 
-                    <div className='container row  d-flex justify-content-between fw-regular mx-auto mt-1'>
-                    {!user && (
-                        <>
-                        <div onClick={logGoogleUser} className="col-4 button-google d-flex flex-row gap-2 w-auto justify-content-center align-items-center py-2 px-2">
-                            <img  src={ logogoogle } className=' ' width={15} alt="" /> Entrar con Google
-                        </div>
-                        <div onClick={logGoogleUser}  className="col-4 button-apple d-flex flex-row gap-2 w-auto justify-content-center align-items-center py-2 px-2">
-                            <img  src={ logoapple } className='' width={15} alt="" /> Entrar con Apple
-                        </div>
-                        <div className="col-4 button-google d-flex flex-row w-auto justify-content-center align-items-center py-2 px-2">
-                            <button className='button-google' onClick={() => setIsRegistering(!isRegistering)}>
-                                {isRegistering ? 'Tienes cuenta? Inicia Sesión' : 'No tienes cuenta? Regístrate'}
-                            </button>
-                        </div>
-                        </>
-                    )}
+                    <div className='d-flex flex-column gap-2 justify-content-center align-items-center'>
+                        {!user && (
+                            <>
+                                {isSignUpMode ?
+                                    <div className="row mx-auto w-75">
+                                        <div onClick={loginGoogle} className="col-12 button-google d-flex flex-row justify-content-center align-items-center py-3 gap-4 px-3">
+                                            <img src={logogoogle} className=' ' width={20} alt="" /> Entrar con Google
+                                        </div>
+                                    </div> 
+                                    :
+                                    null
+                                }
+                                <div className="col-4 d-flex flex-row w-auto mx-auto justify-content-center align-items-center py-2 px-2">
+                                    <a className='text-black-50' onClick={() => setIsSignUpMode(!isSignUpMode)}>
+                                        {isSignUpMode ? '¿No tienes cuenta? Cree una aquí' : 'Ya tienes cuenta? Inicia Sesión'}
+                                    </a>
+                                </div>
+                            </>
+                        )}
                     </div>
 
                 </div>
