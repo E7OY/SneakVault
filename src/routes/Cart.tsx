46be10d813a6850daaf7bca203041b31a1c77778
soyel <soyeloygarciamartinez@gmail.com>
2025-03-09 23:00:03 +0100

    fix cart styles errros

--- src/routes/Cart.tsx
+++ src/routes/Cart.tsx
@@ -5,85 +5,81 @@ import mastercad from '../assets/mastercard_icon.webp';
 import visa from '../assets/visa_icon.webp';
 import paypal from '../assets/paypal_icon.webp';
 import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import UserContext from '../context/userContext';
+import { Container } from 'react-bootstrap';
 
 interface CartProps {
     toggleCart: () => void;
 }
 
 export function Cart({ toggleCart }: CartProps) {
-
     const { cart, removeFromCart, increaseQuantity, decreaseQuantity, totalCart } = useCart();
     const userContext = useContext(UserContext);
     const user = userContext?.user;
 
     return (
         <>
-            {user ?
+            {user ? (
+                <Container>
+                    <div className='d-flex flex-row p-5 m-5' onClick={toggleCart}>
+                        <div className="col-8 cart-products">
+                            <h3 className='fw-semibold'>Cesta ({cart.length})</h3>
+                            <ul className='list-unstyled p-0'>
+                                {cart.map((product) => (
+                                    <li key={product.id} className='my-5'>
+                                        <div>
+                                            <div className="w-100 product-details d-flex flex-row justify-content-around align-items-center float-start">
+                                                <img src={imageMap[product.name]} width={200} alt={product.name} />
+                                                <Link to={`/${product.categoria}/${product.marca}/${product.id}`} className='text-decoration-none'>
+                                                    <h5 className='w-100 fw-light text-black text-decoration-none'>
+                                                        {product.name}<br />
+                                                        <span className='fw-light text-black-50'>{product.price}€</span>
+                                                    </h5>
+                                                </Link>
+                                                <h6 className='w-auto fw-light text-black'>{product.marca}</h6>
+                                                <h6 className='p-3 border border-1 fw-light border-black float-end'>{product.quantity}</h6>
+                                            </div>
+                                            <div className='d-flex w-100 flex-row justify-content-end gap-1 align-items-center'>
+                                                <button onClick={() => decreaseQuantity(product.id)} className='p-2 px-3 fw-regular border border-1 fw-light border-black bg-transparent'>-</button>
+                                                <button onClick={() => increaseQuantity(product.id)} className='p-2 px-3 fw-regular border border-1 fw-light border-black bg-transparent'>+</button>
+                                                <button onClick={() => removeFromCart(product.id)} className='p-2 px-3 fw-regular border border-1 fw-light border-black bg-negro text-white'>Eliminar</button>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
 
-
-        <div className="d-flex justify-content-center" onClick={toggleCart}>
-            <div className='cart border border-1 border-black m-5'>
-                <h4 className='fw-light'>Cesta  ({cart.length})</h4>
-                <ul className='list-unstyled p-0'>
-                    {cart.map((product) => (
-                        <li key={product.id} className='my-5'>
-                            <div>
-                                <div className="w-100 product-details d-flex flex-row justify-content-around align-items-center float-start">
-                                    <img src={imageMap[product.name]} width={200} alt={product.name} />
-                                    <h6 className='w-50 fw-light text-black'>{product.name}
-                                    <h6 className='fw-light text-black-50'>{product.price}€</h6></h6>
-                                    <h6 className='p-3 border border-1 fw-light border-black float-end'>{product.quantity}</h6>
-                                </div>
-                                <div className='d-flex w-100 flex-row justify-content-end gap-1 align-items-center'>
-                                    <button onClick={() => decreaseQuantity(product.id)} className='p-2 px-3 fw-regular border border-1 fw-light border-black bg-transparent'>-</button>
-                                    <button onClick={() =>  increaseQuantity(product.id)} className='p-2 px-3 fw-regular border border-1 fw-light border-black bg-transparent'>+</button>
-                                    <button onClick={() => removeFromCart(product.id)} className='p-2 px-3 fw-regular border border-1 fw-light border-black bg-negro text-white'>Eliminar</button>
-                                </div>
-                            </div>
+                        <div className="col-4 cart-payment">
+                            <h4 className='fw-light'>Resumen</h4>
+                            <h5 className='fw-light'>
+                                Subtotal
+                                <span className='d-inline float-end text-black-50 fw-light'>€{totalCart()} EUR</span>
+                            </h5>
+                            <h5 className='fw-light'>
+                                Entrega
+                                <span className='d-inline float-end text-black-50 fw-light'>Gratis</span>
+                            </h5>
                             <hr />
-                        </li>
-                        
-                    ))}
-                </ul>
+                            <h4 className='fw-semibold'>
+                                Total
+                                <span className='d-inline float-end text-black-50 fw-light'>€{totalCart()} EUR</span>
+                            </h4>
 
-                <h4 className='fw-light'>Total<h4 className='d-inline float-end text-black-50 fw-light'> €{totalCart()} EUR</h4>
-
-                </h4>
-                <button className='p-3 px-5 my-3 fw-regular border border-1 fw-light border-black bg-negro text-white'>Comprar</button>
-                <div className='d-flex flex-row gap-1'>
-                    <img src={mastercad} className='p-1 border-1 border border-black-50' width={50} alt="imagen logo mastercard"/>
-                    <img src={visa} className='p-1 border-1 border border-black-50' width={50} alt="imagen logo visa"/>
-                    <img src={paypal} className='p-1 border-1 border border-black-50' width={50} alt="imagen logo paypal"/>
-                </div>
-<<<<<<< HEAD
-=======
-
-                <div className="col-4 cart-payment ">
-                    <h4 className='fw-light'>Resumen</h4>
-                    <h5 className='fw-light'>Subtotal<h5 className='d-inline float-end text-black-50 fw-light'> €{totalCart()} EUR</h5></h5>
-                    <h5 className='fw-light'>Entrega<h5 className='d-inline float-end text-black-50 fw-light'>Gratis</h5></h5>
-                    <hr />
-                    <h4 className='fw-semibold'>Total<h4 className='d-inline float-end text-black-50 fw-light'>€{totalCart()} EUR</h4></h4>
-
-
-                    <button className='p-3 px-5 my-3 w-100 fw-regular border border-1 fw-light border-black bg-negro text-white'>Comprar</button>
-                    <div className='d-flex flex-row justify-content-start gap-1'>
-                        <img src={mastercad} className='p-1 border-1 border border-black-50' width={50} alt="imagen logo mastercard"/>
-                        <img src={visa} className='p-1 border-1 border border-black-50' width={50} alt="imagen logo visa"/>
-                        <img src={paypal} className='p-1 border-1 border border-black-50' width={50} alt="imagen logo paypal"/>
+                            <button className='p-3 px-5 my-3 w-100 fw-regular border border-1 fw-light border-black bg-negro text-white'>Comprar</button>
+                            <div className='d-flex flex-row justify-content-start gap-1'>
+                                <img src={mastercad} className='p-1 border-1 border border-black-50' width={50} alt="imagen logo mastercard" />
+                                <img src={visa} className='p-1 border-1 border border-black-50' width={50} alt="imagen logo visa" />
+                                <img src={paypal} className='p-1 border-1 border border-black-50' width={50} alt="imagen logo paypal" />
+                            </div>
+                        </div>
                     </div>
-                </div>
-
->>>>>>> 98e0d5158eb67b493cd5be49705a3cd7cb959e89
-            </div>
-        </div>
-
-
-    :
-    <Navigate to="/register" />
-    }
+                </Container>
+            ) : (
+                <Navigate to="/register" />
+            )}
         </>
     );
 }
