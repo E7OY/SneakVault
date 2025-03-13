381b9761d76105eaabe763b00c424e46e9606f4d
soyel <soyeloygarciamartinez@gmail.com>
2025-03-13 10:29:38 +0100

    add clearcart method to context

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -8,6 +8,7 @@ import carrito from '../../assets/carrito.webp';
 import { onValue, ref } from "firebase/database";
 import { db } from "../../utils/firebase.utils";
 import CategoryMenu from "../CategoryMenu";
+import CartContext from "../../context/cartContext";
 
 interface Category {
     name: string;
@@ -18,18 +19,21 @@ const NavBar = () => {
     const [searchInput, setSearchInput] = useState('');
     const navigate = useNavigate();
     const location = useLocation();
+    const cartContext = useContext(CartContext);
+    const cart = cartContext?.cart;
+    const clearCart = cartContext?.clearCart;
+    // creamos un estado para guardar las categorias que obtendremos de la base de datos
     const [categories, setCategories] = useState<Category[]>([]);
-    const userContext = useContext(UserContext);
-    const user = userContext ? userContext.user : null;
-    const setUser = userContext ? userContext.setUser : () => { };
 
     useEffect(() => {
+        //obtener categorias de la base de datos de forma dinamica
         const fetchCategories = async () => {
             try {
                 const productosRef = ref(db, 'productos');
                 onValue(productosRef, (snapshot) => {
                     const productosData = snapshot.val();
                     if (productosData) {
+                        //mapeamos el objeto de categorias a un array de categorias con sus respectivas marcas para poder mostrarlo en el navbar
                         const categoriesArray: Category[] = Object.entries(productosData).map(([categoryName, categoryData]) => {
                             const brands = Object.keys(categoryData as object);
                             return {
@@ -57,7 +61,7 @@ const NavBar = () => {
 
     const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        navigate(`/search-results?query=${encodeURIComponent(searchInput)}`);
+        navigate(`/search?query=${encodeURIComponent(searchInput)}`);
     };
 
     const handleNavLinkClick = (path: string) => {
@@ -65,19 +69,54 @@ const NavBar = () => {
         setSearchInput('');
     };
 
+    const userContext = useContext(UserContext);
+    const user = userContext ? userContext.user : null;
+    const setUser = userContext ? userContext.setUser : () => { };
+    const errorMessage = document.querySelectorAll('.error-message');
+    const [isCartOpen, setIsCartOpen] = useState(false);
+
     const handleSignOut = async () => {
         try {
             await signOut(getAuth());
             setUser(null);
+            if (clearCart) {
+                clearCart();
+            }
         } catch (error) {
-            console.error("Error signing out:", error);
+            Array.from(errorMessage).forEach((element) => {
+                element.textContent = 'Error al cerrar sesión: ' + error;
+            });
         }
     };
+
+    const toggleCart = () => {
+        setIsCartOpen(!isCartOpen);
+    };
+
+    useEffect(() => {
+        const texts = document.querySelectorAll('.moving-text h6');
+        let index = 0;
+
+        const showNextText = () => {
+            texts.forEach((text, i) => {
+                text.classList.remove('active');
+                if (i === index) {
+                    text.classList.add('active');
+                }
+            });
+            index = (index + 1) % texts.length;
+        };
+
+        showNextText();
+        const interval = setInterval(showNextText, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <Navbar expand="lg" className="bg-white py-3 px-5 border-bottom border-dark">
             <Link to="/home" className="h1 text-decoration-none negro mb-0 fw-light">SneakVault</Link>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" className="border-white rounded-0 text-white-50" />
+            <Navbar.Toggle aria-controls="basic-navbar-na" className="border-white rounded-0 text-white-50" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Container className="navbar d-flex justify-content-between w-100">
                     <Nav className="d-flex flex-column flex-lg-row align-items-lg-left gap-3 w-auto">
@@ -103,13 +142,17 @@ const NavBar = () => {
                                 Cerrar Sesión
                             </button>
                             <Link to="/cart">
-                                <button className="bg-transparent border-1 p-2 border-dark">
+                                <button onClick={toggleCart} className="bg-transparent border-1 p-2 border-dark">
                                     <img src={carrito} width={37} alt="imagen carrito" />
                                 </button>
                             </Link>
                         </>
                     ) : (
-                        <Link to="/login" className="button fw-light" onClick={() => handleNavLinkClick('/login')}>
+                        <Link
+                            to="/login"
+                            className="button fw-light"
+                            onClick={() => handleNavLinkClick('/login')}
+                        >
                             Iniciar Sesión
                         </Link>
                     )}
@@ -117,6 +160,6 @@ const NavBar = () => {
             </Navbar.Collapse>
         </Navbar>
     );
-};
+}
 
 export default NavBar;
--- src/context/cartContext.tsx
+++ src/context/cartContext.tsx
@@ -1,63 +1,41 @@
 //contexto global para poder manejar el estado del carrito en toda la app
 
-import { createContext, useState, useContext, useEffect } from 'react';
-import { getAuth } from 'firebase/auth';
-import { ref, set, onValue } from 'firebase/database';
-import { db } from '../utils/firebase.utils';
+    import { createContext, useState, useContext, useEffect } from 'react';
 
-interface CartItem {
-    id: string;
-    name: string;
-    price: number;
-    image: string;
-    quantity: number;
-    stock: number;
-    categoria: string;
-    marca: string;
-}
-
-interface CartContextProps {
-    cart: CartItem[];
-    addToCart: (product: { id: string; name: string; price: number; image: string; stock: number; categoria: string; marca: string }) => void;
-    removeFromCart: (productId: string) => boolean;
-    increaseQuantity: (productId: string) => void;
-    decreaseQuantity: (productId: string) => void;
-    totalCart: () => number;
-}
-
-const CartContext = createContext<CartContextProps | undefined>(undefined);
-
-export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [cart, setCart] = useState<CartItem[]>([]);
-    const auth = getAuth();
-    const user = auth.currentUser;
-
-    // Recuperar el carrito desde Firebase al cargar la página
-    useEffect(() => {
-        const auth = getAuth();
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            if (user) {
-                const cartRef = ref(db, `carts/${user.uid}`);
-                onValue(cartRef, (snapshot) => {
-                    const cartData = snapshot.val();
-                    if (cartData) {
-                        setCart(cartData);
-                    }
-                });
-            }
+    interface CartItem {
+        id: string;
+        name: string;
+        price: number;
+        image: string;
+        quantity: number;
+        stock: number;
+        categoria: string;
+        marca: string;
+    }
+    
+    interface CartContextProps {
+        cart: CartItem[];
+        setCart: (cart: CartItem[]) => void;
+        addToCart: (product: { id: string; name: string; price: number; image: string; stock: number; categoria: string; marca: string }) => void;
+        removeFromCart: (productId: string) => boolean;
+        increaseQuantity: (productId: string) => void;
+        decreaseQuantity: (productId: string) => void;
+        totalCart: () => number;
+        clearCart: () => void;
+    }
+    
+    const CartContext = createContext<CartContextProps | undefined>(undefined);
+    
+    export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+        const [cart, setCart] = useState<CartItem[]>(() => {
+            const savedCart = localStorage.getItem('cart');
+            return savedCart ? JSON.parse(savedCart) : [];
         });
-
-        return () => unsubscribe();
-    }, []);
-
-    // Guardar el carrito en Firebase cada vez que se actualice
-    const saveCartToDatabase = (updatedCart: CartItem[]) => {
-        if (user) {
-            const cartRef = ref(db, `carts/${user.uid}`);
-            set(cartRef, updatedCart);
-        }
-    };
-
+    
+        useEffect(() => {
+            localStorage.setItem('cart', JSON.stringify(cart));
+        }, [cart]);
+    
     //AÑADIR PRODUCTO
     const addToCart = (product: { id: string; name: string; price: number; image: string; stock: number; categoria: string; marca: string }) => {
         //crear una copia del carrito
@@ -74,21 +52,19 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children
         }
 
         setCart(updatedCart);
-        saveCartToDatabase(updatedCart);
     };
-
+    
     //ELIMINAR PRODUCTO
     const removeFromCart = (productId: string) => {
         if (window.confirm("¿Seguro que quieres eliminar este producto del carrito?")) {
             //filtramos el carrito para que no incluya el producto que queremos eliminar
             const updatedCart = cart.filter(item => item.id !== productId);
             setCart(updatedCart);
-            saveCartToDatabase(updatedCart);
             return true;
         }
         return false;
     };
-
+    
     //AUMENTAR CANTIDAD DE PRODUCTO
     const increaseQuantity = (productId: string) => {
         //crear una copia del carrito
@@ -99,10 +75,9 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children
         if (productToUpdate) {
             productToUpdate.quantity += 1;
             setCart(updatedCart);
-            saveCartToDatabase(updatedCart);
         }
     };
-
+    
     //DISMINUIR CANTIDAD DE PRODUCTO
     const decreaseQuantity = (productId: string) => {
         //crear una copia del carrito
@@ -113,10 +88,9 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children
         if (productToUpdate && productToUpdate.quantity > 1) {
             productToUpdate.quantity -= 1;
             setCart(updatedCart);
-            saveCartToDatabase(updatedCart);
         }
     };
-
+    
     //TOTAL DEL CARRITO
     const totalCart = () => {
         let total = 0;
@@ -128,15 +102,20 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children
         //return cart.reduce((total, item) => total + item.price * item.quantity, 0);
         return total;
     };
-
+    
+    //LIMPIAR CARRITO
+    const clearCart = () => {
+        setCart([]);
+    };
+    
     //provider que nos permite usar el contexto global con las funciones y el estado
-    return (
-        <CartContext.Provider value={{ cart, addToCart, removeFromCart, increaseQuantity, decreaseQuantity, totalCart }}>
-            {children}
-        </CartContext.Provider>
-    );
-};
-
-//el hook para poder user el contexto global
-export const useCart = () => useContext(CartContext);
-export default CartContext;
+        return (
+            <CartContext.Provider value={{ cart, addToCart, removeFromCart, increaseQuantity, decreaseQuantity, totalCart, clearCart }}>
+                {children}
+            </CartContext.Provider>
+        );
+    };
+    
+    //el hook para poder user el contexto global
+    export const useCart = () => useContext(CartContext);
+    export default CartContext;
