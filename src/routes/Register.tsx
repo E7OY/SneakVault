7912c0291d489fd47593ab595856db73e479516d
EloyGarcia <soyeloygarciamartinez@gmail.com>
2025-03-14 18:19:01 +0100

    add forms validations and quit names files from gitignore

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -3,8 +3,7 @@ import show from '../assets/show.webp';
 import hide from '../assets/hide.webp';
 import { Form, Navigate, useNavigate } from "react-router-dom";
 import UserContext from "../context/userContext";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification } from "firebase/auth";
 
 const Register = () => {
     const [email, setEmail] = useState('');
@@ -15,10 +14,12 @@ const Register = () => {
     const [showPassword, setShowPassword] = useState(false);
     const navigate = useNavigate();
     const userContext = useContext(UserContext);
+    const [errors, setErrors] = useState<string[]>([]);
     // user es el usuario actual, si no hay usuario es null
     const user = userContext ? userContext.user : null;
     const setUser = userContext ? userContext.setUser : () => { };
     const errorMessage = document.getElementsByClassName('error');
+
     //mostrar contraseña
     const showPassWord = () => {
         setShowPassword(!showPassword);
@@ -28,112 +29,163 @@ const Register = () => {
     //funcion asincrona con evento de formulario
     const register = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();  //para evitar el comportamiento default del evento, del form, (enviarlo y recargar pag)
+
+        if (!formValidation()) {
+            return;
+        }
+
         try {
             const auth = getAuth();
             const { user } = await createUserWithEmailAndPassword(auth, email, password);
+            await sendEmailVerification(user);  //email de verificacion
             setUser(user);
-            navigate(-1);
-        } catch {
-            Array.from(errorMessage).forEach((element) => {
-                element.textContent = 'Error en el registro.';
-            });
+            navigate('/home');
+        } catch (error) {
+            setErrors(["Error en el registro. Por favor, inténtalo de nuevo."]);
         }
     };
 
+    //validaciones formulario
+    const formValidation = (): boolean => {
+        const newErrors: string[] = [];
+
+        if (!username.trim()) {
+            newErrors.push("Escribe un nombre de usuario");
+        }
+        if (password.length < 6) {
+            newErrors.push("La contraseña debe contener al menos 6 caracteres.");
+        }
+        if (!name.trim()) {
+            newErrors.push("Escribe un nombre.");
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            newErrors.push("El email escrito no es válido.");
+        }
+        const birthDate = new Date(date);
+        const today = new Date();
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDiff = today.getMonth() - birthDate.getMonth();
+        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        if (age < 18) {
+            newErrors.push("Debes ser mayor de edad para registrarte!");
+        }
+
+        setErrors(newErrors);
+        return newErrors.length === 0;
+    };
+
     return (
-
         <>
-
             <div className="register my-5 p-5">
                 <div className="row mx-auto">
                     <div className="mx-auto col-12 col-md-6 border border-1 border-black py-5 d-flex text-center flex-column justify-content-center">
                         <h3 className='fw-light mt-4'>CREAR UNA CUENTA</h3>
-                        <h6 className='my-3 fw-light'>Registrate y consigue un 10% de descuento</h6>
+                        <h6 className='my-3 fw-light'>Regístrate y consigue un 10% de descuento</h6>
                         {user ? (
-                        <Navigate to='/home' replace />
-                    ) : (
-                        <Form method='post' onSubmit={register}>
-                            <div className='form-field'>
-                                <label htmlFor='gender'><h6 className='fw-light text-black-50'>Género </h6></label>
-                                <div className='form-field w-75 d-flex justify-content-center flex-row gap-4 mx-auto'>
-                                    <div>
-                                        <input type='radio' name='gender' id='hombre' value='Hombre' />
-                                        <label htmlFor='hombre'><h6 className='text-black-50 mx-1 fw-lighter'>Hombre</h6></label>
-                                    </div>
-                                    <div>
-                                        <input type='radio' name='gender' id='mujer' value='Mujer' />
-                                        <label htmlFor='mujer'><h6 className='text-black-50 mx-1 fw-lighter'>Mujer</h6></label>
-                                    </div>
-                                    <div>
-                                        <input type='radio' name='gender' id='otro' value='Otro' />
-                                        <label htmlFor='otro'><h6 className='text-black-50 mx-1 fw-lighter'>Otro</h6></label>
-                                    </div>
-                                </div>
-                            </div>
+                            <Navigate to='/home' replace />
+                        ) : (
+                            <Form method='post' onSubmit={register}>
 
-                            <div className='form-field my-4 w-75 d-flex gap-0 mx-auto'>
-                                <label htmlFor='email' id='email'><h6 className='fw-light text-black-50'>Email *</h6></label>
-                                <input type='text' className='p-1 fw-light' name='email' id='email' required value={email} onChange={(e) => setEmail(e.target.value)} />
-                            </div>
+                                {errors.length > 0 && (
+                                    <div className="error my-3">
+                                        {errors.map((error, index) => (
+                                            <p key={index} className="text-danger">{error}</p>
+                                        ))}
+                                    </div>
+                                )}
 
-                            <div className='form-field my-4 w-75 d-flex gap-0 mx-auto'>
-                                <label htmlFor='password' id='password'><h6 className='fw-light text-black-50'>Contraseña *</h6></label>
-                                <div>
+                                <div className='form-field my-4 w-75 d-flex gap-0 mx-auto'>
+                                    <label htmlFor='email' id='email'><h6 className='fw-light text-black-50'>Email *</h6></label>
                                     <input
-                                        type={showPassword ? 'text' : 'password'}
-                                        className='p-1 w-75 fw-light'
-                                        name='password'
-                                        id='password'
+                                        type='text'
+                                        className='p-1 fw-light'
+                                        name='email'
+                                        id='email'
                                         required
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
                                     />
-                                    <button type='button' className='w-25 button-password py-1' onClick={showPassWord}>
-                                        <img width={20} src={showPassword ? hide : show} alt='hide / show password' />
-                                    </button>
                                 </div>
-                            </div>
 
-                            <div className='form-field my-4 w-75 d-flex gap-0 mx-auto'>
-                                <label htmlFor='username' id='username'><h6 className='fw-light text-black-50'>Nombre de Usuario *</h6></label>
-                                <input type='text' className='p-1 fw-light' name='username' id='username' required value={username} onChange={(e) => setUsername(e.target.value)} />
-                            </div>
-                            <div className='form-field my-4 w-75 d-flex gap-0 mx-auto'>
-                                <label htmlFor='name' id='name'><h6 className='fw-light text-black-50'>Nombre y apellidos *</h6></label>
-                                <input type='text' className='p-1 fw-light' name='name' id='name' required value={name} onChange={(e) => setName(e.target.value)} />
-                            </div>
-                            <div className='form-field  w-75 d-flex gap-0 mx-auto'>
-                                <label htmlFor='date' id='date'><h6 className='fw-light text-black-50'>Fecha de Nacimiento *</h6></label>
-                                <input type='date' className='p-1 fw-light' name='date' id='date' required value={date} onChange={(e) => setDate(e.target.value)} />
-                            </div>
+                                <div className='form-field my-4 w-75 d-flex gap-0 mx-auto'>
+                                    <label htmlFor='password' id='password'><h6 className='fw-light text-black-50'>Contraseña *</h6></label>
+                                    <div>
+                                        <input
+                                            type={showPassword ? 'text' : 'password'}
+                                            className='p-1 w-75 fw-light'
+                                            name='password'
+                                            id='password'
+                                            required
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                        />
+                                        <button type='button' className='w-25 button-password py-1' onClick={showPassWord}>
+                                            <img width={20} src={showPassword ? hide : show} alt='hide / show password' />
+                                        </button>
+                                    </div>
+                                </div>
 
-                            <div className='form-field w-75 my-4 d-flex justify-content-start align-items-start flex-row gap-1 mx-auto'>
-                                <input type='checkbox' name='terms' id='terms' />
-                                <label htmlFor='terms'><h6 className='text-black-50 mx-1 fw-light'>Acepto los terminos y condiciones.</h6></label>
-                            </div>
+                                <div className='form-field my-4 w-75 d-flex gap-0 mx-auto'>
+                                    <label htmlFor='username' id='username'><h6 className='fw-light text-black-50'>Nombre de Usuario *</h6></label>
+                                    <input
+                                        type='text'
+                                        className='p-1 fw-light'
+                                        name='username'
+                                        id='username'
+                                        required
+                                        value={username}
+                                        onChange={(e) => setUsername(e.target.value)}
+                                    />
+                                </div>
 
+                                <div className='form-field my-4 w-75 d-flex gap-0 mx-auto'>
+                                    <label htmlFor='name' id='name'><h6 className='fw-light text-black-50'>Nombre y apellidos *</h6></label>
+                                    <input
+                                        type='text'
+                                        className='p-1 fw-light'
+                                        name='name'
+                                        id='name'
+                                        required
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                    />
+                                </div>
 
-                            <div className="error"></div>
+                                <div className='form-field  w-75 d-flex gap-0 mx-auto'>
+                                    <label htmlFor='date' id='date'><h6 className='fw-light text-black-50'>Fecha de Nacimiento *</h6></label>
+                                    <input
+                                        type='date'
+                                        className='p-1 fw-light'
+                                        name='date'
+                                        id='date'
+                                        required
+                                        value={date}
+                                        onChange={(e) => setDate(e.target.value)}
+                                    />
+                                </div>
+
+                                <div className='form-field w-75 my-4 d-flex justify-content-start align-items-start flex-row gap-1 mx-auto'>
+                                    <input type='checkbox' name='terms' id='terms' required />
+                                    <label htmlFor='terms'><h6 className='text-black-50 mx-1 fw-light'>Acepto los términos y condiciones.</h6></label>
+                                </div>
+
                                 <button type='submit' className='button fw-light w-75 mx-auto'>Registrarse</button>
-                            
-                            <div className="col-12 d-flex flex-row w-auto mx-auto justify-content-center align-items-center py-2 mt-3 px-2">
-                                <a className='text-black-50 fw-light text-decoration-none' onClick={() => navigate("/login")}>
-                                    Ya tienes cuenta? <span className="text-decoration-underline">Inicia Sesión</span>
-                                </a>
-                            </div>
 
-                        </Form> )}
-
+                                <div className="col-12 d-flex flex-row w-auto mx-auto justify-content-center align-items-center py-2 mt-3 px-2">
+                                    <a className='text-black-50 fw-light text-decoration-none' onClick={() => navigate("/login")}>
+                                        ¿Ya tienes cuenta? <span className="text-decoration-underline">Inicia Sesión</span>
+                                    </a>
+                                </div>
+                            </Form>
+                        )}
                     </div>
                 </div>
             </div>
-
-
         </>
-
     );
-
-
-}
+};
 
 export default Register;
